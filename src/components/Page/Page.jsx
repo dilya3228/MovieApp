934954d573b90ce7a93a2ac999281608ee3f289a
Delirious <alex.zik3@mail.ru>
2022-12-12 22:50:20 +0300

    update-services

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -2,18 +2,19 @@ import { Pagination } from 'antd'
 import { useState } from 'react'
 
 const ListPagination = ({ datas, getAllMovies }) => {
-  const { total_results } = datas
+  const { total_results, page } = datas
   const [current, setCurrent] = useState(1)
   const handlePaginate = (numbers) => {
     setCurrent(numbers)
     getAllMovies(numbers)
   }
+
   return (
     <div className="footer paginate">
       <Pagination
         showSizeChanger={false}
-        defaultPageSize={20}
-        defaultCurrent={1}
+        defaultPageSize={1}
+        defaultCurrent={page}
         total={total_results}
         onChange={handlePaginate}
         current={current}
--- src/components/RatedList/RatedList.jsx
+++ src/components/RatedList/RatedList.jsx
@@ -1,28 +1,11 @@
-import { useState } from 'react'
 import { Rate } from 'antd'
-import MovieRequest from '../../sevices/movie-request'
+import movieRateStars from '../../sevices/movie-rate-stars'
 
 const RatedList = ({ id }) => {
-  const [starFull, setStarFull] = useState(0)
-
-  const movieRateStars = async (starFull) => {
-    const data = await MovieRequest.post(
-      `/movie/${id}/rating`,
-      {
-        value: starFull,
-      },
-      {
-        params: {
-          guest_session_id: localStorage.getItem('guest'),
-        },
-      }
-    ).catch((e) => console.log(e.name))
-  }
-
   const handleClickStar = (star) => {
-    setStarFull(star)
-    movieRateStars(star)
+    movieRateStars(star, id)
   }
+
   const localGetRating = localStorage.getItem('str')
   const ratedCards = JSON.parse(localGetRating)
 
@@ -34,6 +17,6 @@ const RatedList = ({ id }) => {
   }
   const aver = res.length > 0 ? res[0].rating : 0
 
-  return <Rate count={10} defaultValue={aver} className="stars" onChange={handleClickStar} starFull={starFull} />
+  return <Rate count={10} defaultValue={aver} className="stars" onChange={handleClickStar} />
 }
 export default RatedList
